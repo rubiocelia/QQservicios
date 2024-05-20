0b4384972c05fa95097feedf517e8af2761e3988
Celia <rubiopaiscelia@gmail.com>
2024-05-20 11:31:17 +0200

    .

--- src/server/insertar_datos_registro_formulario.php
+++ src/server/insertar_datos_registro_formulario.php
@@ -7,12 +7,13 @@ $nombre = $_POST['Nombre'];
 $apellidos = $_POST['Apellidos'];
 $correo = $_POST['correo_electronico'];
 $telefono = $_POST['NumTel'];
+$organizacion = $_POST['Organizacion'];
 $contrasena = $_POST['contrasena']; // Se obtiene la contraseña del formulario
 $conexion = getConexion();
 
 // Preparar la consulta SQL para insertar en la tabla Usuarios
-$sql_usuarios = "INSERT INTO Usuarios (Nombre, Apellidos, Correo_electronico, Numero_telefono, Foto, Fecha_Registro)
-VALUES ('$nombre', '$apellidos', '$correo', '$telefono', 'URL_FotoEjemploUsuarioNuevo', NOW())";
+$sql_usuarios = "INSERT INTO Usuarios (Nombre, Apellidos, Correo_electronico, Numero_telefono,Organizacion, Foto, Fecha_Registro)
+VALUES ('$nombre', '$apellidos', '$correo', '$telefono','$organizacion', './archivos/perfil/fotosPerfil/fotoPorDefecto.jpg', NOW())";
 
 // Realizar la inserción en la tabla Usuarios
 if ($conexion->query($sql_usuarios) === TRUE) {
